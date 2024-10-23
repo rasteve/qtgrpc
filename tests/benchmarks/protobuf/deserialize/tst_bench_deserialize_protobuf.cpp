f78028cea332a2004264fb062e5fd6e70bd856c2
Alexey Edelev <alexey.edelev@qt.io>
2024-10-23 10:13:36 +0200

    Protobuf: Add JSON deserializer benchmark
    Add the benchmark of the JSON deserialzer. Re use the common
    functionality in both JSON and protobuf deserializer benchmarks.
    
    Pick-to: 6.8 6.9
    Task-number: QTBUG-128812
    Change-Id: I34befe253d076793228f660b24a9983af0fbc780
    Reviewed-by: Dennis Oberst <dennis.oberst@qt.io>

--- tests/benchmarks/protobuf/deserialize/tst_bench_deserialize_protobuf.cpp
+++ tests/benchmarks/protobuf/deserialize/tst_bench_deserialize_protobuf.cpp
@@ -5,21 +5,15 @@
 
 #include <QtProtobuf/qprotobufserializer.h>
 
-#include "bench.qpb.h"
+#include "tst_bench_deserialize_protobuf_base.h"
 
 using namespace Qt::StringLiterals;
 
-class tst_ProtoDeserialize : public QObject
+class ProtobufDeserializeBenchmark : public ProtobufDeserializeBenchmarkBase
 {
     Q_OBJECT
-
-private Q_SLOTS:
-    void initTestCase();
-    void deserialize_data();
-    void deserialize();
-
-private:
-    QByteArray benchBuffer;
+public:
+    ProtobufDeserializeBenchmark();
 };
 
 std::array<std::pair<QString, QByteArray>, 3> data = {
@@ -30,79 +24,11 @@ std::array<std::pair<QString, QByteArray>, 3> data = {
                    QByteArray::fromHex("080112120802120e0803120a08041206080512020806")),
 };
 
-void tst_ProtoDeserialize::initTestCase()
+ProtobufDeserializeBenchmark::ProtobufDeserializeBenchmark()
+    : ProtobufDeserializeBenchmarkBase(&data, std::make_unique<QProtobufSerializer>())
 {
-    qtbench::ScalarTypes s;
-    s.setField1(std::numeric_limits<QtProtobuf::int32>::min());
-    s.setField2(std::numeric_limits<QtProtobuf::uint32>::max());
-    s.setField3(std::numeric_limits<QtProtobuf::sint32>::max());
-    s.setField4(std::numeric_limits<QtProtobuf::fixed32>::max());
-    s.setField5(std::numeric_limits<QtProtobuf::sfixed32>::min());
-    s.setField6(std::numeric_limits<QtProtobuf::int64>::min());
-    s.setField7(std::numeric_limits<QtProtobuf::uint64>::max());
-    s.setField8(std::numeric_limits<QtProtobuf::sint64>::min());
-    s.setField9(std::numeric_limits<QtProtobuf::fixed64>::max());
-    s.setField10(std::numeric_limits<QtProtobuf::sfixed64>::min());
-    s.setField11(42.0);
-    s.setField12(42.0);
-    s.setField13("Hello Qt!"_L1);
-    s.setField14("Hello Qt!"_ba);
-    s.setField15(qtbench::ScalarTypes::EnumType::Value3);
-
-    qtbench::ScalarTypesRepeated r;
-    r.setField1({ s, s, s, s, s, s, s, s, s, s, });
-    r.setField2({ qtbench::ScalarTypes::EnumType::Value1, qtbench::ScalarTypes::EnumType::Value2,
-                  qtbench::ScalarTypes::EnumType::Value3, qtbench::ScalarTypes::EnumType::Value3,
-                  qtbench::ScalarTypes::EnumType::Value2, qtbench::ScalarTypes::EnumType::Value1,
-                  qtbench::ScalarTypes::EnumType::Value2, qtbench::ScalarTypes::EnumType::Value3,
-                  qtbench::ScalarTypes::EnumType::Value1, qtbench::ScalarTypes::EnumType::Value2,
-                  qtbench::ScalarTypes::EnumType::Value1, qtbench::ScalarTypes::EnumType::Value3 });
-
-    qtbench::MapTypes m;
-    m.setField1({
-        { 0,                                             r },
-        { std::numeric_limits<QtProtobuf::int32>::max(), r },
-        { std::numeric_limits<QtProtobuf::int32>::min(), r }
-    });
-    m.setField2({
-        { ""_L1,           r },
-        { "Hellow Qt!"_L1, r }
-    });
-
-    qtbench::BenchmarkMessage msg;
-    msg.setField1({ m });
-
-    QProtobufSerializer serializer;
-    benchBuffer = msg.serialize(&serializer);
 }
 
-void tst_ProtoDeserialize::deserialize_data()
-{
-    QTest::addColumn<QString>("name");
-    QTest::addColumn<QByteArray>("data");
-
-    for (auto &&[name, value] : data)
-        QTest::newRow(qPrintable(name)) << name << value;
-
-    for (auto i : {1, 10, 100, 1000}) {
-        QTest::addRow("qtbench.BenchmarkMessage x %d", i)
-            << "qtbench.BenchmarkMessage" << benchBuffer.repeated(i);
-    }
-}
-
-void tst_ProtoDeserialize::deserialize()
-{
-    QFETCH(QString, name);
-    QFETCH(QByteArray, data);
-
-    QProtobufSerializer serializer;
-    QBENCHMARK {
-        QProtobufMessagePointer res = QProtobufMessage::constructByName(name);
-        QVERIFY(res);
-        QVERIFY(res->deserialize(&serializer, data));
-    }
-}
-
-QTEST_MAIN(tst_ProtoDeserialize)
+QTEST_MAIN(ProtobufDeserializeBenchmark)
 
 #include "tst_bench_deserialize_protobuf.moc"

21232748622c1e438213fbb770349488867293d5
Dennis Oberst <dennis.oberst@qt.io>
2024-12-13 14:13:45 +0100

    Docs: simplify qt-add-grpc-example.cmake
    This is one of the first introductions in how to use the cmake api. Try
    to start teaching the most simplest way: reusing the application target.
    
    Pick-to: 6.9 6.8
    Change-Id: I94c070bde4dec5adf33f4f244cad0ed105c4d510
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>

--- src/grpc/doc/src/snippets/qt-add-grpc-example.cmake
+++ src/grpc/doc/src/snippets/qt-add-grpc-example.cmake
@@ -5,21 +5,17 @@ project(MyProject)
 find_package(Qt6 REQUIRED COMPONENTS Protobuf Grpc)
 qt_standard_project_setup()
 
-qt_add_protobuf(MyProtoMessageLib
-    PROTO_FILES
-        path/to/helloworld.proto
-    PROTO_INCLUDES
-        path/to/proto/include
-)
-
-qt_add_grpc(MyGrpcClient CLIENT
-    PROTO_FILES
-        path/to/helloworld.proto
-    PROTO_INCLUDES
-        path/to/proto/include
-)
-
 qt_add_executable(MyApp main.cpp)
 
-target_link_libraries(MyApp PRIVATE MyGrpcClient MyProtoMessageLib Qt6::Protobuf)
+qt_add_protobuf(MyApp
+    PROTO_FILES
+        path/to/messages.proto
+)
+
+qt_add_grpc(MyApp CLIENT
+    PROTO_FILES
+        path/to/service.proto
+)
+
+target_link_libraries(MyApp PRIVATE Qt6::Protobuf Qt6::Grpc)
 #! [0]
